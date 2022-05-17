420a84a1e819dec55dfa78a8706bc867f1de5840
zhutiankang <zhutiankang@smartlink-tech.com.cn>
2022-05-17 19:05:06 +0800

    词频1

--- app/src/main/kotlin/base/processor/ProcessorV1.kt
+++ app/src/main/kotlin/base/processor/ProcessorV1.kt
@@ -1,5 +1,7 @@
 package base.processor
 
+import java.io.File
+
 /**
  * ProcessorV1
  *
@@ -9,9 +11,54 @@ package base.processor
  */
 class ProcessorV1 {
 
-//    fun processText(text: String): List<WordFreq> {
-//
-//    }
+    fun processText(text: String): List<WordFreq> {
+
+        // 1.文本清洗
+        val cleaned = clean(text)
+        // 2.文本分割
+        val words = cleaned.split(" ")
+        // 3.统计单词频率
+        val map = getWordCount(words)
+        // 4.词频排序
+        val list = sortByFrequency(map)
+
+        return list
+    }
+
+    fun clean(text: String): String {
+        return text.replace("[^A-Za-z]".toRegex(), " ").trim()
+    }
+
+    fun getWordCount(list: List<String>): Map<String, Int> {
+        val map = hashMapOf<String, Int>()
+        for (word in list) {
+            if (word == "") continue
+            val trim = word.trim()
+            val count = map.getOrDefault(trim, 0)
+            map[trim] = count + 1
+        }
+        return map
+    }
+
+    fun sortByFrequency(map: Map<String, Int>): MutableList<WordFreq> {
+
+        val list = mutableListOf<WordFreq>()
+        for (entry in map) {
+            if (entry.key == "") continue
+            val freq = WordFreq(entry.key, entry.value)
+            list.add(freq)
+        }
+        list.sortByDescending {
+            it.frequency
+        }
+        return list
+    }
+
+    fun processFile(file: File): List<WordFreq> {
+        val text = file.readText(Charsets.UTF_8)
+        return processText(text)
+    }
 }
+
 
 data class WordFreq(val word: String, val frequency: Int)
