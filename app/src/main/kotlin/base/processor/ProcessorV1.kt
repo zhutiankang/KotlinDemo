0f602561bede4107270cdac53f749b9d45dad879
zhutiankang <zhutiankang@smartlink-tech.com.cn>
2022-06-09 16:44:44 +0800

    词频2

--- app/src/main/kotlin/base/processor/ProcessorV1.kt
+++ app/src/main/kotlin/base/processor/ProcessorV1.kt
@@ -62,3 +62,55 @@ class ProcessorV1 {
 
 
 data class WordFreq(val word: String, val frequency: Int)
+
+class ProcessorV2 {
+
+    fun processText(text: String): List<WordFreq> {
+        return text
+            .clean()
+            .split(" ")
+            .getWordCount()
+            .mapToList { WordFreq(it.key, it.value) }
+            .sortedByDescending { it.frequency }
+    }
+
+    fun String.clean(): String {
+        return this.replace("[^A-Za-z]".toRegex(), " ").trim()
+    }
+
+    fun List<String>.getWordCount(): Map<String, Int> {
+        val map = HashMap<String, Int>()
+        for (element in this) {
+            if (element == "") continue
+            val trim = element.trim()
+            val count = map.getOrDefault(element, 0)
+            map[trim] = count + 1
+        }
+        return map
+    }
+
+    fun Map<String, Int>.sortByFrequency(): MutableList<WordFreq> {
+        val list = mutableListOf<WordFreq>()
+        for (entry in this) {
+            if (entry.key == "") continue
+            val freq = WordFreq(entry.key, entry.value)
+            list.add(freq)
+        }
+        list.sortByDescending {
+            it.frequency
+        }
+        return list
+    }
+
+    //将 inline 用于修饰高阶函数
+    //inline 的作用其实就是将 inline 函数当中的代码拷贝到调用处
+    inline fun <T> Map<String, Int>.mapToList(transform: (Map.Entry<String, Int>) -> T): MutableList<T> {
+        val list = mutableListOf<T>()
+        for (entry in this) {
+            val freq = transform(entry)
+            list.add(freq)
+        }
+        return list
+    }
+
+}
