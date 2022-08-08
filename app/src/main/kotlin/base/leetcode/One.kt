91b53b541297b4aecda763da20011293fc2662b0
zhutiankang <zhutiankang@smartlink-tech.com.cn>
2022-08-08 20:18:10 +0800

    leetcode one

---
+++ app/src/main/kotlin/base/leetcode/One.kt
@@ -0,0 +1,43 @@
+package base.leetcode
+
+/**
+ * One
+ *
+ * @author tiankang
+ * @description:
+ * @date :2022/8/8 17:38
+ */
+
+// 程序的输入是一个字符串 s。题目要求我们移除当中的所有元音字母 a、e、i、o、u，然后返回。
+fun removeVowels(s: String): String = s.filter {
+    it !in setOf('a', 'e', 'i', 'o')
+}
+
+//程序的输入是一段英语文本（paragraph），一个禁用单词列表（banned）返回出现次数最多、同时不在禁用列表中的单词
+fun mostCommonWord1(paragraph: String, banned: Array<String>) =
+    paragraph.toLowerCase()//非 清理符号
+        .replace("[^a-zA-Z ]".toRegex(), " ")
+        .split("\\s+".toRegex())
+        .filter {
+            it !in banned.toSet()
+        }
+        .groupBy { it }
+        .mapValues { it.value.size }
+        .maxByOrNull {
+            it.value
+        }
+        ?.key ?: throw IllegalArgumentException()
+
+// 冒泡排序
+fun sort(array: IntArray): IntArray {
+    for (end in (array.size - 1) downTo 1) {
+        for (begin in 1..end) {
+            if (array[begin - 1] > array[begin]) {
+                val temp = array[begin - 1]
+                array[begin - 1] = array[begin]
+                array[begin] = temp
+            }
+        }
+    }
+    return array
+}
