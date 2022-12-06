f3387c87fd8d54500f37dbf11a159ffa03953d74
zhutiankang <zhutiankang@smartlink-tech.com.cn>
2022-12-06 18:46:54 +0800

    Compose Demo

---
+++ app/src/main/java/com/github/kotlin/compose/ComposeActivity.kt
@@ -0,0 +1,112 @@
+package com.github.kotlin.compose
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.kotlin.R
+import com.github.kotlin.compose.ui.theme.KotlinDemoTheme
+
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+//                    Greeting(Message("Java", "Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
+                }
+            }
+        }
+    }
+}
+
+//组合函数
+@Composable
+fun Greeting(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                )
+            }
+        }
+    }
+
+}
+
+@Preview("Light Mode")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark Mode", showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KotlinDemoTheme {
+        Surface {
+//            Greeting(
+//                msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+//            )
+            Conversation(SampleData.conversationSample)
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        messages.map {
+            item {
+                Greeting(it)
+            }
+        }
+    }
+}
--- app/src/main/java/com/github/kotlin/ui/GitHubActivity.kt
+++ app/src/main/java/com/github/kotlin/ui/GitHubActivity.kt
@@ -9,6 +9,11 @@ import com.github.kotlin.databinding.ActivityGitHubBinding
 import com.github.kotlin.data.entities.RepoList
 import com.github.kotlin.viewmodel.GitHubViewModel
 
+/**
+ * MVVM+Clean架构的组合使得原先在VM里的数据处理逻辑转移到了usecase，这样VM的逻辑更加精简且清晰，并且由于clean架构的引入，数据层的代码将更加可测。但是，clean架构的缺点也很明显：代码臃肿、结构复杂。实际项目中很少会使用clean架构来实现一个业务模块。
+ * 慢就是快，技术成长没有捷径
+ * 虽然我们每个人的起点各不相同，但终点都是殊途同归的。我们唯一能做的，就是享受其中的过程，去追求来自内心的那份宁静和快乐。与其活在世俗的教条当中让自己难受，还不如跟随自己的内心，去做一些开心的事情。
+ */
 class GitHubActivity : AppCompatActivity() {
 
     private val viewModel: GitHubViewModel by viewModels()
